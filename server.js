eb9e92b41f54ac21eb9e1ad6f4b6a0b02888edc0
Andre Gibson <andregibson@math>
2019-04-29 09:15:30 -0700

    Add index.html for serving tests

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = 1203
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.use(express.static('public'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
